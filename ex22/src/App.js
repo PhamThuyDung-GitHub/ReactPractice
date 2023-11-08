d093c14122842026f0d3da17c90e4363579c5990
amenosakura <114124231+amenosakura@users.noreply.github.com>
2023-11-09 00:48:55 +0700

    ex22

--- ex22/src/App.js
+++ ex22/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const [name, setName] = React.useState("Adam");
+  const [age, setAge] = React.useState(35);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <p>My name is {name}</p>
+      </section>
+      <section>
+        <input type="number" value={age} onChange={(e) => setAge(e.target.value)} />
+        <p>My age is {age}</p>
+      </section>
+    </>
   );
 }
 
